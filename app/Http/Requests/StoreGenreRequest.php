5430faed1656b5646bddae27bf45443d2383be1a
stefanenache12 <stefanenache12@gmail.com>
2023-09-25 16:31:42 +0200

    ADD CRUD ALBUM

--- app/Http/Requests/StoreGenreRequest.php
+++ app/Http/Requests/StoreGenreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreGenreRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreGenreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
@@ -22,7 +23,7 @@ class StoreGenreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            
         ];
     }
 }
--- app/Models/Album.php
+++ app/Models/Album.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'year',
+        'description',
+        'country'
+    ];
 }
